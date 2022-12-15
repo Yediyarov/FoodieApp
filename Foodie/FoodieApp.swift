f3771b87c62f09c0a85eed32f7016576114331e5
Yediyarov <yediyarov.xeyal@gmail.com>
2022-12-15 15:43:19 +0400

    initial commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Khayal Yediyarov on 15.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+}
