aebd1db07034020c70f94c168ce6b3523bf9a2b2
Yediyarov <yediyarov.xeyal@gmail.com>
2022-12-19 14:53:20 +0400

    Using the Array of Restaurant Objects

---
+++ Restaurant.swift
@@ -0,0 +1,30 @@
+//
+//  Restaurant.swift
+//  Foodie
+//
+//  Created by Khayal Yediyarov on 16.12.22.
+//
+
+import Foundation
+
+
+struct Restaurant {
+    
+    var name : String
+    var location: String
+    var type: String
+    var image : String
+    var isFavorite: Bool
+    
+    init(name: String,type: String, location: String, image: String, isFavorite: Bool){
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+    init(){
+        self.init(name: "", type: "", location: "", image: "", isFavorite: false)
+    }
+}
