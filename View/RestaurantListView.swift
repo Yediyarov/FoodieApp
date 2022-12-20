62e0ea13fc89d96ca2106aae327372bf141dc4aa
Yediyarov <yediyarov.xeyal@gmail.com>
2022-12-20 18:27:57 +0400

    changed accent color for navigation

--- View/RestaurantListView.swift
+++ View/RestaurantListView.swift
@@ -69,6 +69,7 @@ struct RestaurantListView: View {
             .navigationTitle("Foodie")
             .navigationBarTitleDisplayMode(.automatic)
         }
+        .accentColor(.white)
     }
 }
 
