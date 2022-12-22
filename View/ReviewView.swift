b6fb5c08de9136f22da2f467eff0cd3bffaa0d5c
Yediyarov <yediyarov.xeyal@gmail.com>
2022-12-22 18:53:38 +0400

    applying a visual blur effect

--- View/ReviewView.swift
+++ View/ReviewView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ReviewView: View {
     
+    var restaurant: Restaurant
+    
     var body: some View {
         ZStack{
-            Color.black.ignoresSafeArea()
             
+            Image(restaurant.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .ignoresSafeArea()
+            
+            Color.black
+                .ignoresSafeArea()
+                .opacity(0.6)
+                .background(.ultraThinMaterial)
+
             HStack{
                 Spacer()
                 VStack{
@@ -45,6 +57,14 @@ struct ReviewView: View {
 }
 struct ReviewView_Previews: PreviewProvider {
     static var previews: some View {
-        ReviewView()
+        ReviewView(restaurant: Restaurant(
+            name: "Cafe Deadend",
+            type: "Coffee & Tea",
+            location: "Robert Robertson, 1234 NW Bobcat Lane, St. Robert",
+            phone: "350-233423",
+            description: "A little gem hidden at the corner of the street is nothing but fantastic! This place is warm and cozy. We open at 7 every morning except Sunday, and close at 9 PM. We offer a variety of coffee drinks and specialties including lattes, cappuccinos, teas, and more. We serve breakfast, lunch, and dinner in an airy open setting. Come over, have a coffee and enjoy a chit-chat with our baristas.",
+            image: "cafedeadend",
+            isFavorite: true)
+        )
     }
 }
